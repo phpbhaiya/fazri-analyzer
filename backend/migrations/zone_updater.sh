05500279612e4f916470c51f60a4cc3211dd867a
Subhadeep Pramanik <codevizag@gmail.com>
2025-12-12 05:08:53 +0530

    Fix dependency scanning path for backend requirements 👀

--- backend/migrations/zone_updater.sh
+++ backend/migrations/zone_updater.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 cd /home/ubuntu/apps/fazri-analyzer/backend/migrations
 source ../venv/bin/activate
-
 while true; do
     echo "$(date): Running realtime update..."
     python3 simple_zone_migration3.py
